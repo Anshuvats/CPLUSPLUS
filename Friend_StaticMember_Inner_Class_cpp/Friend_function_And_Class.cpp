a22fbd6ea4dca49081fc5f7eb7be1a27c221bdc7
ANSHU KUMAR <39401992+Anshuvats@users.noreply.github.com>
2019-06-23 16:46:02 +0530

    Updated file

--- Friend_StaticMember_Inner_Class_cpp/Friend_function_And_Class.cpp
+++ Friend_StaticMember_Inner_Class_cpp/Friend_function_And_Class.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+class Test
+{
+private:
+    int a;
+protected:
+    int b;
+public:
+    int c;
+  friend void fun();  
+};
+void fun()
+{
+    Test t;
+    t.a=15;
+    t.b=10;
+    t.c=5;
+}
+int main()
+{
+    
+}
 
