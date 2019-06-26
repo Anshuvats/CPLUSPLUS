69ff721e24e6144e7a6c6c0f6e8462d16d4eeb7a
ANSHU KUMAR <39401992+Anshuvats@users.noreply.github.com>
2019-06-26 13:17:43 +0530

    Added file

---
+++ C11_cpp/Auto_DeclType.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+float fun()
+{
+    return 2.34f;
+}
+int main()
+{
+  //auto x=2*5.7+'a';
+  auto x=fun();
+  cout<<x;
+  //int x=10;
+  //float y=90.5;
+  //decltype(y)z=x+y;
+}
